3b17eb7423dfb44f4d73ec6a4b8a56620abe40df
Prakash Bhatta <prakashbhatta@Amadablam.local>
2020-05-05 14:21:48 +0545

    Methods

--- p1.rb
+++ p1.rb
@@ -1,11 +1,9 @@
 #  Write a program that prints a greeting message. 
 # This program should contain a method called greeting 
 # that takes a name as its parameter and returns a string.
-
+# Methods prob set.
 
 def greeting(name)
    "Hello " + name + ". How are you doing?"
 end
 puts greeting("bikam")
-
-
